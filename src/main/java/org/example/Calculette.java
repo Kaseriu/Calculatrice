05a349034848e4114a8fa6c6eb1623ba906f0a6a
Kaseriu <amatuszek@myges.fr>
2021-06-07 18:10:00 +0200

    Changes

--- src/main/java/org/example/Calculette.java
+++ src/main/java/org/example/Calculette.java
@@ -3,7 +3,7 @@ package org.example;
 public class Calculette {
 
     public static int add(int a, int b) {
-        return a + b;
+        return a + b + 1;
     }
 
     public static int sub(int a, int b) {
