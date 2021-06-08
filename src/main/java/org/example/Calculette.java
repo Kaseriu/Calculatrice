a32508df48549c6e75dcc515adb9c8280ed0b9eb
Kaseriu <56071607+Kaseriu@users.noreply.github.com>
2021-06-08 17:32:42 +0200

    Update Calculette.java

--- src/main/java/org/example/Calculette.java
+++ src/main/java/org/example/Calculette.java
@@ -3,7 +3,7 @@ package org.example;
 public class Calculette {
 
     public static int add(int a, int b) {
-        return a + b + 1;
+        return a + b;
     }
 
     public static int sub(int a, int b) {
